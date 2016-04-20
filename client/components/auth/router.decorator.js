42e17e56d6055f37055e52d5ad58fec5c916d923
andry-baka <andry.baka@mitrais.com>
2016-04-20 12:59:17 +0800

    Now user will always force to login screen if not login yet. Also combine account.js into main.js

--- client/components/auth/router.decorator.js
+++ client/components/auth/router.decorator.js
@@ -18,7 +18,7 @@ angular.module('meanonlineshopApp.auth')
 
           event.preventDefault();
           return Auth.isLoggedIn(_.noop).then(is => {
-            $state.go(is ? 'main' : 'login');
+            $state.go(is ? 'login' : 'login');
           });
         });
       } else {
@@ -28,7 +28,7 @@ angular.module('meanonlineshopApp.auth')
           }
 
           event.preventDefault();
-          $state.go('main');
+          $state.go('login');
         });
       }
     });
